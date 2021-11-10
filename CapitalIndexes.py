7b4a3c86629023f117de1b9b566e2a89bb5e0f9c
Chrisbmorris21 <88673241+Chrisbmorris21@users.noreply.github.com>
2021-11-10 13:03:20 +0000

    Add files via upload
    This is my attempt at the first Python Principles challenge. If you'd like to try these challenges out, head over to https://www.pythonprinciples.com

---
+++ CapitalIndexes.py
@@ -0,0 +1,27 @@
+#CHALLENGE
+#Take a string, return it as an array with the keys of each uppercase letter.
+#e.g. HeLlO should return 0,2,4
+#e.g. ChiCKeN should return 0,3,4,6
+
+#Thought process
+#Get length of string
+#Create empty array to store keys
+#Split string, iterate throguh each character, check if its upper or lower
+#If capital, append value to empty array
+#Return completed array
+#test function
+
+def capital_indexes(string):
+    characterArray = []
+    x = string.split()
+    for i in range(len(x)):
+        y = str(x[i])
+        for i in range(len(y)):
+            if y[i].isupper():
+                characterArray.append(i)
+                print("ISUPPER")
+                print(i)
+
+        return(characterArray)
+
+print(capital_indexes("HeLlO"))
